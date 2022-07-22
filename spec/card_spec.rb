36a826a00e017875633c42efd5cec5903004a7e9
George Borsato <georginhoborsato@gmail.com>
2022-07-22 17:22:53 -0300

    fix: lint

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,22 +1,22 @@
 class Card
-	attr_reader :rank, :suit
+  attr_reader :rank, :suit
 
-	def initialize(rank, suit)
-		@rank = rank
-		@suit = suit
-	end
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
 end
 
 RSpec.describe Card do
-	def card
-		Card.new('Ace','Spades')
-	end
+  def card
+    Card.new('Ace', 'Spades')
+  end
 
-	it "has a rank" do
-		expect(card.rank).to eq('Ace')
-	end
-	
-	it 'has a suit' do 
-		expect(card.suit).to eq('Spades')
-	end
+  it 'has a rank' do
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
+  end
 end
